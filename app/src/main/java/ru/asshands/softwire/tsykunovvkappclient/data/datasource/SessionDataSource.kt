9791bb0a8b6218a64db0217ea17b0c19ffa7318f
MegaDedh <ToyotaCami813!@#>
2019-05-27 15:50:45 +0700

    Add check Auth token with SharedPreferences

---
+++ app/src/main/java/ru/asshands/softwire/tsykunovvkappclient/data/datasource/SessionDataSource.kt
@@ -0,0 +1,33 @@
+package ru.asshands.softwire.tsykunovvkappclient.data.datasource
+
+import android.content.SharedPreferences
+import io.reactivex.Single
+import ru.asshands.softwire.tsykunovvkappclient.data.network.Api
+import ru.asshands.softwire.tsykunovvkappclient.data.response.ProfileResponse
+import javax.inject.Inject
+
+interface SessionDataSource {
+    fun clearToken()
+    fun getToken(): String
+    fun saveToken(token: String)
+
+}
+
+class SessionDataSourceImpl @Inject constructor(
+    private val sharedPreferences: SharedPreferences
+) : SessionDataSource {
+
+    private companion object {
+        const val KEY_TOKEN = "authToken"
+    }
+
+    override fun saveToken(token: String) = sharedPreferences.edit()
+        .putString(KEY_TOKEN, token)
+        .apply()
+
+    override fun clearToken() = sharedPreferences.edit()
+        .remove(KEY_TOKEN)
+        .apply()
+
+    override fun getToken(): String = sharedPreferences.getString(KEY_TOKEN, "").orEmpty()
+}
